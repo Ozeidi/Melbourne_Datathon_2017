d75e3740124165388168b21826f51c4b0dcab030
Ozeidi <ozeidi91@gmail.com>
2017-04-18 12:06:08 +1000

    Update to use Geocoding library
    Notes:
    1.The previous Nominatim geocdoing service from geopy, prodocued multiple errors in queried postcode. Over 800 postcodes resulted in coordinates for a place called Asutralia in Urguay :~.
    2. To avoid those error I switched to GoogLev3 service also in geopy -where registration and api_key is need from-. The service was apparently  limited  to 1 request/sec and maximum 2500 request per day, which does not satisfy our requirements.
    3. As last resort, I switched to geocode library, which apparently works as a scraper based on  various services. In the code above I used googel service which works well on most the postal codes. Only ~30 post codes gave no coordinators.

--- Patients_Geocoding.py
+++ Patients_Geocoding.py
@@ -1,17 +1,6 @@
-from itertools import islice
-from time import clock
-import pandas as pd
-import os
-
-# if using jupyter:
-%pylab inline
-
-from geopy.geocoders import Nominatim
-geo = Nominatim()
-
-# from mpl_toolkits.basemap import Basemap
-
-#-------------------------------------
+#pip install geocoder
+import geocoder
+#Modify this to your own lookup folder
 lookups = 'D:/Documents/IPython/Datathon/MelbDatathon2017/lookups/'
 df_patients=pd.read_csv('{}patients.txt'.format(lookups), sep = '\t')
 pc=df_patients.postcode.unique()
@@ -19,12 +8,15 @@ pc=df_patients.postcode.unique()
 df_pc=pd.DataFrame(columns=['postcode'])
 df_pc.postcode=pc
 
+gc=Nominatim(view_box=(112.467,-55.050,168.000,-9.133),country_bias='Australia')
+
+
 def get_cor(postcode):
     s_t=clock()
     loc=None
     while loc is None:
         try:
-            loc=geo.geocode('{}, Australia'.format(postcode))
+            loc=geocoder.google('{}, Australia'.format(postcode))
         except:
             print ("error")
         
@@ -32,8 +24,8 @@ def get_cor(postcode):
     time.append(e_t-s_t)
     print('{}:{}'.format(len(time),time[-1]),end=' , ')
     print ('{}'.format(postcode),end=',')
-    print('{},{}'.format(loc.longitude,loc.latitude))
-    return '{}#{}#{}'.format(loc.address,loc.longitude,loc.latitude)
+    print('{},{}'.format(loc.lng,loc.lat))
+    return '{}#{}#{}'.format(loc.address,loc.lng,loc.lat)
 
 
 df_pc=df_pc[:]
@@ -44,6 +36,6 @@ df_pc['address,lon,lat']=df_pc['postcode'].apply(lambda x: get_cor(x))
 df_pc['address'],df_pc['lon'],df_pc['lat']=df_pc['address,lon,lat'].str.split('#').str
 df_pc=df_pc.drop('address,lon,lat',1)
 
-df_pc.lon=df_pc.lon.astype(float)
-df_pc.lat=df_pc.lat.astype(float)
+# df_pc.lon=df_pc.lon.astype(float)
+# df_pc.lat=df_pc.lat.astype(float)
 df_pc
